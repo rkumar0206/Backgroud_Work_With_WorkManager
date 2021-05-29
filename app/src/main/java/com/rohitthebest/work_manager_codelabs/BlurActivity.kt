9d84770618e40b078cdabf77e7e7e4634c9184a6
Rohit <rkumar8092378845@gmail.com>
2021-05-29 15:18:44 +0530

    -> Added a WorkManager's custom configuration -> Disable the default initialization for work manager in AndroidManifest.xml

--- app/src/main/java/com/rohitthebest/work_manager_codelabs/BlurActivity.kt
+++ app/src/main/java/com/rohitthebest/work_manager_codelabs/BlurActivity.kt
@@ -2,7 +2,6 @@ package com.rohitthebest.work_manager_codelabs
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
--- app/src/main/java/com/rohitthebest/work_manager_codelabs/BlurApplication.kt
+++ app/src/main/java/com/rohitthebest/work_manager_codelabs/BlurApplication.kt
@@ -2,9 +2,11 @@ package com.rohitthebest.work_manager_codelabs
 
 import android.app.Application
 import androidx.viewbinding.BuildConfig
+import androidx.work.Configuration
 import timber.log.Timber
 
-class BlurApplication : Application() {
+class BlurApplication : Application(), Configuration.Provider {
+
 
     override fun onCreate() {
         super.onCreate()
@@ -13,5 +15,16 @@ class BlurApplication : Application() {
             Timber.plant(Timber.DebugTree())
         }
 
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+
+        return if(BuildConfig.DEBUG) {
+
+            Configuration.Builder().setMinimumLoggingLevel(android.util.Log.DEBUG).build()
+        }else {
+
+            Configuration.Builder().setMinimumLoggingLevel(android.util.Log.ERROR).build()
+        }
     }
 }
